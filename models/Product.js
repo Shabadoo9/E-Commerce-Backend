31852e6791d3ea8d5b93994863d95f5c1cf04aa2
Shabadoo9 <brinstonzbpe@gmail.com>
2023-11-01 14:39:47 -0400

    final commit

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,8 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-     // defined columns
-     id: {
+    // defined columns
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
